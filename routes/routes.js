c1bd4c686161bc3039f44c9ce5d4c5720e4c03ab
eparedes-dev <erick.paredes.nunez@udla.edu.ec>
2023-07-03 00:33:31 -0500

    Creacion de enrutamientos para el server

--- routes/routes.js
+++ routes/routes.js
@@ -2,3 +2,8 @@ const express = require('express');
 const router = express.Router();
 const getAllVendedores = require('../controllers/vendedoresController');
 const getAllVentas = require('../controllers/ventasController');
+
+router.get('/Vendedores',getAllVendedores);
+router.get('/Ventas',getAllVentas);
+
+module.exports = router;
