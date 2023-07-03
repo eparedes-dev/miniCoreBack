de20b1ef5e33d73f742f8c0a7e8fea9a0782821b
eparedes-dev <erick.paredes.nunez@udla.edu.ec>
2023-07-03 02:11:15 -0500

    Correccion obtencion de datos de Ventas

--- controllers/ventasController.js
+++ controllers/ventasController.js
@@ -1,7 +1,11 @@
 const connection = require('../config/db');
 
 const getAllVentas = (req,result) => {
-    connection.query("select * from Ventas", (error,results)=>{
+    connection.query(`
+    SELECT Ventas.id, Ventas.fecha, Vendedores.nombre AS nombre_vendedor, Ventas.producto, Ventas.monto
+    FROM Ventas
+    JOIN Vendedores ON Ventas.vendedorId = Vendedores.id;
+    `, (error,results)=>{
       if(error){
         result.send(error);
         console.log(error)
