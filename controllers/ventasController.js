aff4d5f35e247bbcdd844676280d57f118ad3b00
eparedes-dev <erick.paredes.nunez@udla.edu.ec>
2023-07-03 15:05:24 -0500

    Correccion de calculo de Monto Total en el API

--- controllers/ventasController.js
+++ controllers/ventasController.js
@@ -15,6 +15,29 @@ const getAllVentas = (req,result) => {
     });
   }
 
+  const getVentasFecha = (req, res) => {
+    const { fechaInicio, fechaFin } = req.body;
+  
+    connection.query(
+      `
+      SELECT Ventas.id, Ventas.fecha, Vendedores.nombre AS nombre_vendedor, Ventas.producto, Ventas.monto
+      FROM Ventas
+      JOIN Vendedores ON Ventas.vendedorId = Vendedores.id
+      WHERE Ventas.fecha BETWEEN ? AND ?;
+      `,
+      [fechaInicio, fechaFin],
+      (error, results) => {
+        if (error) {
+          res.status(500).json({ error: 'Error al obtener las ventas por rango de fechas' });
+        } else {
+          // Calcular el monto total
+          const montoTotal = results.reduce((total, venta) => total + parseFloat(venta.monto), 0);
+  
+          res.json({ ventas: results, montoTotal });
+        }
+      }
+    );
+  };
 
-module.exports = getAllVentas;
+module.exports = {getAllVentas,getVentasFecha};
   
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const getAllVendedores = require('../controllers/vendedoresController');
-const getAllVentas = require('../controllers/ventasController');
+const {getAllVentas,getVentasFecha} = require('../controllers/ventasController');
 
 router.get('/Vendedores',getAllVendedores);
 router.get('/Ventas',getAllVentas);
+router.post('/ventas/fecha', getVentasFecha);
 
 module.exports = router;
